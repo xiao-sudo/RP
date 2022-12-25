5ffeedccbf7bb531e766c24b23b4ecedeca5d459
xiao-sudo <85089925@qq.com>
2022-12-25 22:04:03 +0800

    1. add RenderPipelineAsset

---
+++ Assets/RP/Runtime/RPRenderPipelineAsset.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace RP.Runtime
+{
+    [CreateAssetMenu(menuName = "Rendering/CreateRPPipeline")]
+    public class RPRenderPipelineAsset : RenderPipelineAsset
+    {
+        protected override RenderPipeline CreatePipeline()
+        {
+            return null;
+        }
+    }
+}
