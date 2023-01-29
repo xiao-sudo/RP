06a7853e1fb63a68dcf25f9f37b06b2895ee80a1
xiao-sudo <85089925@qq.com>
2023-01-29 09:08:50 +0800

    1. add CameraRenderer, Setup -> Cull -> Render

---
+++ Assets/RP/Runtime/CameraRenderer.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace RP.Runtime
+{
+    public class CameraRenderer
+    {
+        private const string kBufferName = "RP Render Camera";
+
+        private ScriptableRenderContext m_Context;
+        private Camera m_Camera;
+        private CullingResults m_CullingResults;
+
+        private readonly CommandBuffer m_CommandBuffer = new CommandBuffer()
+        {
+            name = kBufferName
+        };
+
+        public void Render(ScriptableRenderContext context, Camera camera)
+        {
+            m_Context = context;
+            m_Camera = camera;
+
+            if (!Cull())
+                return;
+
+            Setup();
+
+            DrawSky();
+
+            Submit();
+        }
+
+        private void Setup()
+        {
+            m_CommandBuffer.BeginSample(kBufferName);
+
+            // set rt and clear
+            m_CommandBuffer.SetRenderTarget(BuiltinRenderTextureType.CameraTarget);
+            m_CommandBuffer.ClearRenderTarget(true, true, Color.clear);
+
+            ExecuteBuffer();
+            m_Context.SetupCameraProperties(m_Camera);
+        }
+
+        private bool Cull()
+        {
+            if (m_Camera.TryGetCullingParameters(out var p))
+            {
+                m_CullingResults = m_Context.Cull(ref p);
+                return true;
+            }
+
+            return false;
+        }
+
+        private void DrawSky()
+        {
+            m_Context.DrawSkybox(m_Camera);
+        }
+
+
+        private void ExecuteBuffer()
+        {
+            m_Context.ExecuteCommandBuffer(m_CommandBuffer);
+            m_CommandBuffer.Clear();
+        }
+
+        private void Submit()
+        {
+            m_CommandBuffer.EndSample(kBufferName);
+            ExecuteBuffer();
+            m_Context.Submit();
+        }
+    }
+}
--- Assets/RP/Runtime/RPRenderPipeline.cs
+++ Assets/RP/Runtime/RPRenderPipeline.cs
@@ -5,8 +5,12 @@ namespace RP.Runtime
 {
     public class RPRenderPipeline : RenderPipeline
     {
+        private readonly CameraRenderer m_CameraRenderer = new CameraRenderer();
+
         protected override void Render(ScriptableRenderContext context, Camera[] cameras)
         {
+            foreach (var camera in cameras)
+                m_CameraRenderer.Render(context, camera);
         }
     }
 }
