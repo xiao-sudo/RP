8d6b432aed1422ba9d7b1d4f37df4c16a4b20a3b
xiao-sudo <85089925@qq.com>
2023-01-30 21:15:58 +0800

    1. PerObjectMaterialProperties add Cutoff

--- Assets/Examples/PerObjectMaterialProperties.cs
+++ Assets/Examples/PerObjectMaterialProperties.cs
@@ -6,10 +6,15 @@ namespace Examples
     public class PerObjectMaterialProperties : MonoBehaviour
     {
         private static readonly int BASE_COLOR_ID = Shader.PropertyToID("_BaseColor");
+        private static readonly int CUTOFF_ID = Shader.PropertyToID("_Cutoff");
+
         private static MaterialPropertyBlock MATERIAL_PROPERTY_BLOCK;
 
         [SerializeField]
         private Color m_BaseColor = Color.white;
+
+        [SerializeField, Range(0, 1f)]
+        private float m_Cutoff = 0.5f;
 
         private void OnValidate()
         {
@@ -21,6 +26,8 @@ namespace Examples
                     MATERIAL_PROPERTY_BLOCK = new MaterialPropertyBlock();
 
                 MATERIAL_PROPERTY_BLOCK.SetColor(BASE_COLOR_ID, m_BaseColor);
+                MATERIAL_PROPERTY_BLOCK.SetFloat(CUTOFF_ID, m_Cutoff);
+
                 renderer_instance.SetPropertyBlock(MATERIAL_PROPERTY_BLOCK);
             }
         }
