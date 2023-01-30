c5648368ca1b1aac17588bdcd0001d3fb134c1d2
xiao-sudo <85089925@qq.com>
2023-01-30 20:18:02 +0800

    1. add texture 2. add blend and zwrite

--- Assets/Shaders/UnlitPass.hlsl
+++ Assets/Shaders/UnlitPass.hlsl
@@ -3,19 +3,25 @@
 
 #include "../ShaderLibrary/RPCommon.hlsl"
 
+TEXTURE2D(_BaseMap);
+SAMPLER(sampler_BaseMap);
+
 UNITY_INSTANCING_BUFFER_START(UnityPerMaterial)
 UNITY_DEFINE_INSTANCED_PROP(float4, _BaseColor)
+UNITY_DEFINE_INSTANCED_PROP(float4, _BaseMap_ST)
 UNITY_INSTANCING_BUFFER_END(UnityPerMaterial)
 
 struct Attributes
 {
     float3 position_os : POSITION;
+    float2 base_uv : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
 struct Varyings
 {
     float4 position_cs : SV_POSITION;
+    float2 base_uv : TEXCOORD0;
     UNITY_VERTEX_INPUT_INSTANCE_ID
 };
 
@@ -28,13 +34,19 @@ Varyings UnlitPassVertex(Attributes input)
     const float3 position_ws = TransformObjectToWorld(input.position_os);
     output.position_cs = TransformWorldToHClip(position_ws);
 
+    float4 base_st = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseMap_ST);
+    output.base_uv = input.base_uv * base_st.xy + base_st.zw;
+
     return output;
 }
 
 half4 UnlitPassFragment(Varyings input) : SV_TARGET
 {
     UNITY_SETUP_INSTANCE_ID(input);
-    return UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+    float4 base_map = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, input.base_uv);
+    const float4 base_color = UNITY_ACCESS_INSTANCED_PROP(UnityPerMaterial, _BaseColor);
+
+    return base_map * base_color;
 }
 
 #endif
